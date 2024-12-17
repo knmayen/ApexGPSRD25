6ec549ef0a995da7bfd436a68d6e07c1dbf5ad19
knmayen <kmayen@andrew.cmu.edu>
2024-12-16 23:37:37 -0500

    tried to pickle
    unsucessful, try again tommorow

--- testgpx.py
+++ testgpx.py
@@ -1,8 +1,22 @@
-file = open(r'C:\Users\knmay\OneDrive\Desktop\gps\data.txt', 'r')
+import pickle
 
-lines = file.readlines()
+file = open(r"C:\Users\knmay\OneDrive\Documents\GitHub\ApexGPSRD25\data.txt", 'ab')
 
-for line in lines:
-    print(line)
+# lines = file.readlines()
+
+# for line in lines:
+#     print(line)
+
+var = 'hello'
+pickle.dump(var, file)
+
 
 file.close()
+print('here')
+
+nfile = open(r"C:\Users\knmay\OneDrive\Documents\GitHub\ApexGPSRD25\data.txt", 'rb')
+db = pickle.load(nfile)
+for line in nfile.readlines():
+    print(lines)
+
+nfile.close()
