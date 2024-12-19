24bf4f70d30296e121fab0392f8c8b69549b48f0
knmayen <kmayen@andrew.cmu.edu>
2024-12-18 20:44:42 -0500

    learning about tkinter

--- testgpx.py
+++ testgpx.py
@@ -1,22 +1,36 @@
+import pandas
 import pickle
+import tkinter
+from tkinter import *
 
-file = open(r"C:\Users\knmay\OneDrive\Documents\GitHub\ApexGPSRD25\data.txt", 'ab')
+
+file = pandas.read_csv(r"C:\Users\knmay\OneDrive\Documents\GitHub\ApexGPSRD25\neighborhoodLap.csv")
+print(file)
+
+screen = tkinter.Tk()
+# l = Label(screen, text = 'Hello!')
+# l.pack()
+b = Button(screen, text = 'Click', width = 15)
+b.pack()
+screen.mainloop()
+
+# file = open(r"C:\Users\knmay\OneDrive\Documents\GitHub\ApexGPSRD25\data.txt", 'r')
 
 # lines = file.readlines()
 
 # for line in lines:
 #     print(line)
 
-var = 'hello'
-pickle.dump(var, file)
+# var = 'hello'
+# pickle.dump(var, file)
 
 
-file.close()
-print('here')
+# file.close()
+# print('here')
 
-nfile = open(r"C:\Users\knmay\OneDrive\Documents\GitHub\ApexGPSRD25\data.txt", 'rb')
-db = pickle.load(nfile)
-for line in nfile.readlines():
-    print(lines)
+# nfile = open(r"C:\Users\knmay\OneDrive\Documents\GitHub\ApexGPSRD25\data.txt", 'rb')
+# db = pickle.load(nfile)
+# for line in nfile.readlines():
+#     print(lines)
 
-nfile.close()
+# nfile.close()
