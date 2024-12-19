8c6cd37c8acf6798e6a6325014ea0e9fd907c06f
knmayen <kmayen@andrew.cmu.edu>
2024-12-18 21:13:02 -0500

    testing tkinter maps

--- testgpx.py
+++ testgpx.py
@@ -2,17 +2,32 @@ import pandas
 import pickle
 import tkinter
 from tkinter import *
+import tkintermapview
+import PIL
+from PIL import *
 
 
-file = pandas.read_csv(r"C:\Users\knmay\OneDrive\Documents\GitHub\ApexGPSRD25\neighborhoodLap.csv")
-print(file)
+
+# file = pandas.read_csv(r"C:\Users\knmay\OneDrive\Documents\GitHub\ApexGPSRD25\neighborhoodLap.csv")
+# print(file)
 
 screen = tkinter.Tk()
+screen.geometry(f"{800}x{600}")
+screen.title('map')
+
+mapWidget = tkintermapview.TkinterMapView(screen, width = 800, height = 600)
+mapWidget.place(relx = .5, rely = .5, anchor = tkinter.CENTER)
+
+mapWidget.set_position(40.440224, -79.945156)  
+mapWidget.set_zoom(17)
+
 # l = Label(screen, text = 'Hello!')
 # l.pack()
-b = Button(screen, text = 'Click', width = 15)
-b.pack()
+# b = Button(screen, text = 'Click', width = 15)
+# b.pack()
 screen.mainloop()
+
+
 
 # file = open(r"C:\Users\knmay\OneDrive\Documents\GitHub\ApexGPSRD25\data.txt", 'r')
 
