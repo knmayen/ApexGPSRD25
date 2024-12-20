0425a61220c9d0faa6a391eceecbc9fb1f7dd763
knmayen <kmayen@andrew.cmu.edu>
2024-12-19 20:04:03 -0500

    Update mappingUI.py
    Has both the marker and the polygon code

--- mappingUI.py
+++ mappingUI.py
@@ -19,26 +19,32 @@ infoDict = {'rollNum' : 1,
 roll1 = Roll(file, infoDict)
 
 
-# current_path = os.path.join(os.path.dirname(os.path.abspath(__file__)))
-# dotIcon = ImageTk.PhotoImage(Image.open(r"C:\Users\knmay\OneDrive\Documents\GitHub\ApexGPSRD25\redDot.png"))
-
-
-
 
 ################################### SCREEN STUFF #######################################
 
 def drawPoints(self):
+
+    # current_path = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+    # dotIcon = ImageTk.PhotoImage(Image.open(os.path.join(current_path, r"C:\Users\knmay\OneDrive\Documents\GitHub\ApexGPSRD25\redDot.png")).resize((5, 5)))
+    L = []
     for row in self.gpx.itertuples():
         lat = row[3]
         lon = row[4]
-        marker = mapWidget.set_marker(lat, lon)
-        # marker.change_icon()
+        # marker = mapWidget.set_marker(lat, lon, text = '', icon = dotIcon)
+        L.append((lat, lon))
+    rev = L[::-1]
+    L.extend(rev)
+    # print(L)
+    polygon = mapWidget.set_polygon(L, fill_color = None, outline_color = 'red', border_width = 1)
+
 
 screen = tkinter.Tk()
 screen.geometry(f"{800}x{600}")
 screen.title('map')
 
 mapWidget = tkintermapview.TkinterMapView(screen, width = 800, height = 600)
+mapWidget.pack(fill = 'both', expand = True)
+
 mapWidget.place(relx = .5, rely = .5, anchor = tkinter.CENTER)
 
 mapWidget.set_position(41.648136, -70.491481)  
